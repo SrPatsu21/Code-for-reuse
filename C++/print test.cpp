2eb11a635957c97ee041104769d2363df884ca35
Paulo Sérgio Pierdoná <109459945+SrPatsu21@users.noreply.github.com>
2024-02-13 14:05:38 -0300

    Add files via upload

---
+++ C++/print test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// \n and endl is the same thing
+// \" \\ \t
+
+int main() {
+  int x;
+  cout << "\tline 1  \n\n" << endl;
+  cout << "\\ line 2 \"";
+  cin >> x;
+  cout << x;
+  return 0;
+}
---
+++ C++/std omitted.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string greeting = "Hello World";
+    std::cout << greeting;
+    return 0;
+}
