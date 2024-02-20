af723edea64b3ae870f2a43537bd61db330fd18b
SrPatsu21 <paulosergiopierdona@gmail.com>
2024-02-20 09:57:36 -0300

    abstract

---
+++ C++/learning/Abstract.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+//abstract
+class AB {
+    public:
+    virtual void f() = 0;
+};
+
+//abstract
+struct A {
+  virtual void f() = 0;
+};
+
+//testiing 
+struct B : A {
+  virtual void f() {
+    cout << "hello";
+   }
+};
+struct C : AB {
+  virtual void f() {
+    cout << "hello";
+   }
+};
+
+class D : AB
+{
+public:
+    virtual void f() {
+        cout << "hello";
+    }
+};
+class E : A
+{
+public:
+    virtual void f() {
+        cout << "hello";
+    }
+};
+
+//* block null
+A& i(A&);
+
+int main() {
+
+   A* pa;
+   B b;
+   C c;
+   D d;
+   E e;
+
+    b.f();
+    c.f();
+    d.f();
+    e.f();
+}
