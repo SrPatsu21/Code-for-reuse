e56c399033ad55107f8eef02d70b94bd404453b0
SrPatsu21 <paulosergiopierdona@gmail.com>
2024-05-15 09:03:43 -0300

    change device

--- C/tree/Huffman/main.c
+++ C/tree/Huffman/main.c
@@ -68,7 +68,15 @@ void printTree(TREE* tree, int level)
     for (int i = 10; i < level; i++) {
         printf(" ");
     }
-    printf("(%i %c)", tree->priority, tree->varchar);
+    if ('\n' != tree->varchar)
+    {
+        printf("(%i %c)", tree->priority, tree->varchar);
+    }else
+    {
+        printf("(%i \\n)", tree->priority);
+    }
+    
+    
 
     printTree(tree->left, level);
 };
@@ -83,15 +91,60 @@ void setCodifyTable(char table[ASCII_SIZE][(ASCII_SIZE/2)], TREE* tree, int i, c
             {
                 table[((int) tree->varchar)][j] = c[j];
             }
+            printf("\n");
         }else
         {
             i++;
-            c[i] = 0;
+            c[i] = '0';
             setCodifyTable(table, tree->left, i, c);
-            c[i] = 1;
+            c[i] = '1';
             setCodifyTable(table, tree->right, i, c);
         }
     }
+};
+//! make all values of char vet null
+void nullVetChar(char c[(ASCII_SIZE/2)], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        c[i] = '\0';
+    }
+    
+};
+//! make all values of char array null
+void nullArrayChar(char c[ASCII_SIZE][(ASCII_SIZE/2)], int h, int l)
+{
+    for (int j = 0; j < h; j++)
+    {    
+        for (int i = 0; i < l; i++)
+        {
+            c[j][i] = '\0';
+        }
+    }
+};
+
+void printArray(char c[ASCII_SIZE][(ASCII_SIZE/2)], int h, int l)
+{
+    int count= 0;
+    printf("\n");
+    for (int j = 0; j < h; j++)
+    {    
+        if (3 < count)
+        {
+            printf("\n");
+            count = 0;
+        }else
+        {
+            count++;
+        }
+        printf("(%i | ", j);
+        for (int i = 0; i < l; i++)
+        {
+            printf("%c", c[j][i]);
+        }
+        printf(")  ");
+    }
+    printf("\n");
 };
 
 int main(void)
@@ -101,12 +154,22 @@ int main(void)
     LIST* list = createListOfPriority(priority);
     TREE* tree = createTreeBasedOnPriority(list);
     printTree(tree, 0);
-    //set all as \0
+
     char table[ASCII_SIZE][(ASCII_SIZE/2)];
-    //set all as \0
+    nullArrayChar(table, ASCII_SIZE, (ASCII_SIZE/2));
     char c[(ASCII_SIZE/2)];
+    nullVetChar(c, (ASCII_SIZE/2));
 
     setCodifyTable(table, tree, -1, c);
-    
+    printArray(table, ASCII_SIZE, (ASCII_SIZE/2));
+
+    /*
+    *   close file
+    */ 
     fclose(fread);
+    /*
+    *   free tree and list
+    */
+   freeTree(tree);
+   freeList(list);
 }
