276b8a518c322e46e73ee8818ce22df91afe2be4
Paulo Sérgio Pierdoná <paulosergiopierdona@gmail.com>
2024-05-17 20:11:15 -0300

    just work with char

--- C/tree/Huffman/main.c
+++ C/tree/Huffman/main.c
@@ -95,11 +95,18 @@ void setCodifyTable(char table[ASCII_SIZE][(ASCII_SIZE/2)], TREE* tree, int i, c
             }
         }else
         {
+            char copy1[(ASCII_SIZE/2)];
+            char copy2[(ASCII_SIZE/2)];
+            for (size_t j = 0; j < (ASCII_SIZE/2); j++)
+            {
+                copy2[j] = c[j];
+                copy1[j] =  c[j];
+            }
             i++;
-            c[i] = '0';
-            setCodifyTable(table, tree->left, i, c);
-            c[i] = '1';
-            setCodifyTable(table, tree->right, i, c);
+            copy1[i] = '0';
+            setCodifyTable(table, tree->left, i, copy1);
+            copy2[i] = '1';
+            setCodifyTable(table, tree->right, i, copy2);
         }
     }
 };
@@ -150,64 +157,103 @@ void printArray(char c[ASCII_SIZE][(ASCII_SIZE/2)], int h, int l)
 
 void zipFile(FILE* f2read, FILE* f2write, char table[ASCII_SIZE][(ASCII_SIZE/2)])
 {
-    int char1 = 0;
-    int count = 0;
+    // int char1 = 0;
+    // int count = 0;
+    // while (!feof(f2read))
+    // {
+    //     char* input = table[((int)fgetc(f2read))];
+    //     for (int j = 0; VOID_CHAR != input[j]; j++)
+    //     {
+    //         if ('1' == input[j])
+    //         {
+    //             char1 += 1;
+    //         }
+    //         char1 = char1 << 1;
+    //         count++;
+    //         if (7 == count)
+    //         {
+    //             char c = (char)char1;
+    //             fwrite(&c, 1, 1, f2write);
+    //             char1 = 0;
+    //             count = 0;
+    //         }
+            
+    //     }
+    // }
+    // if (0 != count)
+    // {
+    //     char c = (char)char1;
+    //     fwrite(&c, 1, 1, f2write);
+    //     char1 = 0;
+    //     count = 0;
+    // }
+
+    /*
+    * by char
+    */
     while (!feof(f2read))
     {
-        char* input = table[((int)fgetc(f2read))];
-        for (int j = 0; VOID_CHAR != input[j]; j++)
+        int i = ((int)fgetc(f2read));
+        if (i > 0)
         {
-            if ('1' == input[j])
+            for (int j = 0; VOID_CHAR != table[i][j]; j++)
             {
-                //shift
-                char1 <<= 1;
-                char1 += 1;
-                count++;
-            }else if ('0' == input[j])
-            {
-                //shift
-                char1 <<= 1;
-                count++;
+                if ('1' == table[i][j])
+                {
+                    fputc('1', f2write);
+                }else if ('0' == table[i][j])
+                {
+                    fputc('0', f2write);
+                }
             }
-            if (8 == count)
-            {
-                char c = (char)char1;
-                fwrite(&c, 1, 1, f2write);
-                char1 = 0;
-                count = 0;
-            }
-            
         }
     }
 }
 
 void unzipFile(FILE* fzip, FILE* funzip, TREE* tree)
 {
-    int helper = 0;
-    TREE* aux = tree;
-    while (!feof(fzip))
-    {
-        fread(&helper, 1, 1, fzip);
-        
-        for (size_t i = 0; i < 8; i++)
-        {
-            if (NULL != aux)
-            {
-                if ((helper&1))
-                {
-                    aux = aux->right;
-                }else
-                {
-                    aux = aux->left;
-                }
-                helper = helper >> 1;
-            }
+    // int helper = 0;
+    // TREE* aux = tree;
+    // while (!feof(fzip))
+    // {  
+    //     fread(&helper, 1, 1, fzip);
+    //     for (size_t i = 0; i < 8; i++)
+    //     {
+    //         if (aux->left == NULL || aux->right == NULL)
+    //         {
+    //             fputc(aux->varchar, funzip);
+    //             aux = tree;
+    //         }
+    //         if (helper&256)
+    //         {
+    //             aux = aux->right;
+    //         }else
+    //         {
+    //             aux = aux->left;
+    //         }
+    //         helper = helper << 1;
+    //     }
+    // }
 
-            if (aux->left == NULL || aux->right == NULL)
-            {
-                fputc(aux->varchar, funzip);
-                aux = tree;
-            }
+   /*
+   * by char
+   */
+    TREE* aux = tree;
+    char helper = '\0';
+    while (!feof(fzip))
+    {  
+        if (NULL == aux->left || NULL == aux->right)
+        {
+            fputc(aux->varchar, funzip);
+            aux = tree;
+        }
+        helper = fgetc(fzip);
+        if ('1' == helper)
+        {
+            aux = aux->right;
+        }else if ('0' == helper)
+        {
+            aux = aux->left;
         }
     }
 };
@@ -215,19 +261,20 @@ void unzipFile(FILE* fzip, FILE* funzip, TREE* tree)
 int main(void)
 {
     FILE* f2read = fopen("./files/1text.txt", "r");
-    FILE* f2write = fopen("./files/2compacted", "wb");
+    FILE* f2write = fopen("./files/2compacted", "w");
     int* priority = setPriorityArray(f2read);
     LIST* list = createListOfPriority(priority);
     TREE* tree = createTreeBasedOnPriority(list);
-    // printTree(tree, 0);
+    printTree(tree, 0);
 
     char table[ASCII_SIZE][(ASCII_SIZE/2)];
     nullArrayChar(table, ASCII_SIZE, (ASCII_SIZE/2));
     char c[(ASCII_SIZE/2)];
     nullVetChar(c, (ASCII_SIZE/2));
 
+    printf("\n");
     setCodifyTable(table, tree, -1, c);
-    // printArray(table, ASCII_SIZE, (ASCII_SIZE/2));
+    printArray(table, ASCII_SIZE, (ASCII_SIZE/2));
 
     /*
     * write file
@@ -244,7 +291,7 @@ int main(void)
     * unzip
     */
     FILE* funzip = fopen("./files/3unzip.txt", "w");
-    f2write = fopen("./files/2compacted", "rb");
+    f2write = fopen("./files/2compacted", "r");
     rewind(f2write);
     rewind(funzip);
     unzipFile(f2write, funzip, tree);
